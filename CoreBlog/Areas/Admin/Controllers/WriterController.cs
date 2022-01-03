f294bf4ed3543b380b86695a0cb45cc77a26a9fb
İbrahim Külekci <ibrahimkulekci@outlook.com>
2022-01-03 13:52:39 +0300

    Admin panel ajax işlemleri yapılıyor. Ders:109 bitti.

---
+++ CoreBlog/Areas/Admin/Controllers/WriterController.cs
@@ -0,0 +1,41 @@
+﻿using CoreBlog.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreBlog.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class WriterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+
+        public IActionResult WriterGetById(int WriterId)
+        {
+            var findWriter = writers.FirstOrDefault(x => x.Id == WriterId);
+            var jsonWriters = JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriters);
+        }
+
+        public static List<WriterClass> writers = new List<WriterClass>
+        {
+            new WriterClass {Id=1, Name="Ayşe"},
+            new WriterClass {Id=2, Name="Ali"},
+            new WriterClass {Id=3, Name="Veli"},
+            new WriterClass {Id=4, Name="Ahmet"},
+            new WriterClass {Id=5, Name="Osman"}
+        };
+    }
+}
--- CoreBlog/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
+++ CoreBlog/Areas/Admin/ViewComponents/Statistic/Statistic1.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace CoreBlog.Areas.Admin.ViewComponents.Statistic
 {
@@ -18,6 +19,15 @@ namespace CoreBlog.Areas.Admin.ViewComponents.Statistic
             ViewBag.v1 = bm.GetList().Count();
             ViewBag.v2 = c.Contacts.Count();
             ViewBag.v3 = c.Comments.Count();
+
+
+            string api = "e02b83c909ff263baaf39d69b066feab";
+            string connection = "https://api.openweathermap.org/data/2.5/weather?q=gebze&mode=xml&lang=tr&units=metric&appid=" + api;
+            XDocument document = XDocument.Load(connection);
+            ViewBag.v4 = document.Descendants("temperature").ElementAt(0).Attribute("value").Value;
+
+
+
             return View();
         }
     }
